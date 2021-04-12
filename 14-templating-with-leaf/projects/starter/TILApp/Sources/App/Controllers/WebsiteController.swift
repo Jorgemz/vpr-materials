e485d6b32f81984e4a77d917b471e75ecc7de340
🤨 <jorge_mz@icloud.com>
2021-04-12 12:14:48 -0500

    - logic for Detail Page - link for acronym

--- 14-templating-with-leaf/projects/starter/TILApp/Sources/App/Controllers/WebsiteController.swift
+++ 14-templating-with-leaf/projects/starter/TILApp/Sources/App/Controllers/WebsiteController.swift
@@ -9,8 +9,11 @@ import Vapor
 import Leaf
 
 struct WebsiteController: RouteCollection {
+  
   func boot(routes: RoutesBuilder) throws {
     routes.get(use: indexHandler)
+    routes.get("acronyms", ":acronymID", use: acronymHandler)
+
   }
 
   func indexHandler(_ req: Request) -> EventLoopFuture<View> {
@@ -23,9 +26,31 @@ struct WebsiteController: RouteCollection {
       return req.view.render("index", context)
     })
   }
+  
+  func acronymHandler(_ req: Request) -> EventLoopFuture<View> {
+    Acronym
+      .find(req.parameters.get("acronymID"), on: req.db)
+      .unwrap(or: Abort(.notFound))
+      .flatMap({ acronym in
+        acronym.$user.get(on: req.db)
+          .flatMap({ user in
+            let context = AcronymContext(
+              title: acronym.short,
+              acronym: acronym,
+              user: user)
+            return req.view.render("acronym", context)
+          })
+      })
+  }
 }
 
 struct IndexContext: Encodable {
   let title: String
   let acronyms: [Acronym]?
+}
+
+struct AcronymContext: Encodable {
+  let title: String
+  let acronym: Acronym
+  let user: User
 }

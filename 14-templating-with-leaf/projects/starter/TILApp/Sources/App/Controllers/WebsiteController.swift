5a8850a454710a60c9f14d9c87914337f6b6018e
🤨 <jorge_mz@icloud.com>
2021-04-16 20:48:58 -0500

    - logic for users context - page user acronyms - page all users - add list item "All users"

--- 14-templating-with-leaf/projects/starter/TILApp/Sources/App/Controllers/WebsiteController.swift
+++ 14-templating-with-leaf/projects/starter/TILApp/Sources/App/Controllers/WebsiteController.swift
@@ -13,7 +13,8 @@ struct WebsiteController: RouteCollection {
   func boot(routes: RoutesBuilder) throws {
     routes.get(use: indexHandler)
     routes.get("acronyms", ":acronymID", use: acronymHandler)
-
+    routes.get("users", ":userID", use: userHandler)
+    routes.get("users", use: allUsersHandler)
   }
 
   func indexHandler(_ req: Request) -> EventLoopFuture<View> {
@@ -34,12 +35,44 @@ struct WebsiteController: RouteCollection {
       .flatMap({ acronym in
         acronym.$user.get(on: req.db)
           .flatMap({ user in
-            let context = AcronymContext(
-              title: acronym.short,
-              acronym: acronym,
-              user: user)
+            let context =
+              AcronymContext(
+                title: acronym.short,
+                acronym: acronym,
+                user: user)
             return req.view.render("acronym", context)
           })
+      })
+  }
+  
+  func userHandler(_ req: Request) -> EventLoopFuture<View> {
+    User
+      .find(req.parameters.get("userID"), on: req.db)
+      .unwrap(or: Abort(.notFound))
+      .flatMap({ user in
+        user
+          .$acronyms.get(on: req.db)
+          .flatMap({ acronyms in
+            let context =
+              UserContext(
+                title: user.name,
+                user: user,
+                acronyms: acronyms)
+            return req.view.render("user", context)
+          })
+      })
+  }
+  
+  func allUsersHandler(_ req: Request) -> EventLoopFuture<View> {
+    User
+      .query(on: req.db)
+      .all()
+      .flatMap({ users in
+        let context =
+          AllUsersContext(
+            title: "All Users",
+            users: users)
+        return req.view.render("allUsers", context)
       })
   }
 }
@@ -53,4 +86,15 @@ struct AcronymContext: Encodable {
   let title: String
   let acronym: Acronym
   let user: User
+}
+
+struct UserContext: Encodable {
+  let title: String // The title of the page, which is the user’s name.
+  let user: User // The user object to which the page refers.
+  let acronyms: [Acronym] // The acronyms created by this user.
+}
+
+struct AllUsersContext: Encodable {
+  let title: String
+  let users: [User]
 }

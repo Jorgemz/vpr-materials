0b8e82237af78cfebc46619b1dfb2f05e5a70ee6
🤨 <jorge_mz@icloud.com>
2021-04-18 00:40:14 -0500

    - add logic for all categories and details

--- 14-templating-with-leaf/projects/starter/TILApp/Sources/App/Controllers/WebsiteController.swift
+++ 14-templating-with-leaf/projects/starter/TILApp/Sources/App/Controllers/WebsiteController.swift
@@ -15,6 +15,8 @@ struct WebsiteController: RouteCollection {
     routes.get("acronyms", ":acronymID", use: acronymHandler)
     routes.get("users", ":userID", use: userHandler)
     routes.get("users", use: allUsersHandler)
+    routes.get("categories", use: allCategoriesHandler)
+    routes.get("categories", ":categoryID", use: categoryHandler)
   }
 
   func indexHandler(_ req: Request) -> EventLoopFuture<View> {
@@ -74,6 +76,35 @@ struct WebsiteController: RouteCollection {
         return req.view.render("allUsers", context)
       })
   }
+  
+  func allCategoriesHandler(_ req: Request) -> EventLoopFuture<View> {
+    Category
+      .query(on: req.db)
+      .all()
+      .flatMap { categories in
+        let context =
+          AllCategoriesContext(
+            categories: categories)
+        return req.view.render("allCategories", context)
+      }
+  }
+  
+  func categoryHandler(_ req: Request) -> EventLoopFuture<View> {
+    Category
+      .find(req.parameters.get("categoryID"), on: req.db)
+      .unwrap(or: Abort(.notFound))
+      .flatMap { category in
+        category.$acronyms.get(on: req.db).flatMap { acronyms in
+          let context =
+            CategoryContext(
+              title: category.name,
+              category: category,
+              acronyms: acronyms)
+          return req.view.render("category", context)
+        }
+    }
+  }
+
 }
 
 struct IndexContext: Encodable {
@@ -96,4 +127,15 @@ struct UserContext: Encodable {
 struct AllUsersContext: Encodable {
   let title: String
   let users: [User]
+}
+
+struct AllCategoriesContext: Encodable {
+  let title = "All Categories"
+  let categories: [Category]
+}
+
+struct CategoryContext: Encodable {
+  let title: String
+  let category: Category
+  let acronyms: [Acronym]
 }

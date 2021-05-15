000d6d08277c248eb1d8fad8b0e98064b8fb0ba4
🤨 <jorge_mz@icloud.com>
2021-05-15 17:44:23 -0500

    - logic for categories and one category for id

--- 16-making-simple-web-app/projects/starter/TILApp/Sources/App/Controllers/WebsiteController.swift
+++ 16-making-simple-web-app/projects/starter/TILApp/Sources/App/Controllers/WebsiteController.swift
@@ -34,6 +34,8 @@ struct WebsiteController: RouteCollection {
     routes.get("acronyms", ":acronymID", use: acronymHandler)
     routes.get("users", ":userID", use: userHandler)
     routes.get("users", use: allUsersHandler)
+    routes.get("categories", use: allCategoriesHandler)
+    routes.get("categories", ":categoryID", use: categoryHandler)
   }
 
   func indexHandler(_ req: Request) -> EventLoopFuture<View> {
@@ -69,6 +71,26 @@ struct WebsiteController: RouteCollection {
       return req.view.render("allUsers", context)
     }
   }
+  
+  func allCategoriesHandler(_ req: Request) -> EventLoopFuture<View> {
+    Category.query(on: req.db).all().flatMap { categories in
+      let context = AllCategoriesContext(categories: categories)
+      return req.view.render("allCategories", context)
+    }
+  }
+  
+  func categoryHandler(_ req: Request) -> EventLoopFuture<View> {
+    Category.find(req.parameters.get("categoryID"), on: req.db)
+      .unwrap(or: Abort(.notFound)).flatMap { category in
+        category.$acronyms.get(on: req.db).flatMap { acronyms in
+          let context = CategoryContext(
+            title: category.name,
+            category: category,
+            acronyms: acronyms)
+          return req.view.render("category", context)
+        }
+    }
+  }
 }
 
 struct IndexContext: Encodable {
@@ -91,4 +113,15 @@ struct UserContext: Encodable {
 struct AllUsersContext: Encodable {
   let title: String
   let users: [User]
+}
+
+struct AllCategoriesContext: Encodable {
+  let title = "All Categories"
+  let categories: [Category]
+}
+
+struct CategoryContext: Encodable {
+  let title: String
+  let category: Category
+  let acronyms: [Acronym]
 }
